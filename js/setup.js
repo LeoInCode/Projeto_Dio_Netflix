14691d3983b7342517fb0a17101090fca2191530
LeoInCode <lsm.ccomp@gmail.com>
2020-08-27 22:15:50 -0300

    Mostrando a section de capa através do javascript e css

--- js/setup.js
+++ js/setup.js
@@ -15,49 +15,54 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
-const capa0 = document.querySelector('.capa0')
-const capa1 = document.querySelector('.capa1')
-const capa2 = document.querySelector('.capa2')
-const capa3 = document.querySelector('.capa3')
-const capa4 = document.querySelector('.capa4')
-const capa5 = document.querySelector('.capa5')
-const capa6 = document.querySelector('.capa6')
-const capa7 = document.querySelector('.capa7')
-const capa8 = document.querySelector('.capa8')
-const capa9 = document.querySelector('.capa9')
 
+/*Nome das classes de capa que estão na section do html*/
+capasHtml = ['.capa0','.capa1','.capa2','.capa3','.capa4','.capa5','.capa6','.capa7','.capa8','.capa9']
+
+/*variável que será atribuída cada elemento das classes acima*/
+let capa
+
+/*Nomes que estão sendo referenciados no css*/
 capas = ['foto-capa0','foto-capa1','foto-capa2','foto-capa3','foto-capa4','foto-capa5','foto-capa6','foto-capa7','foto-capa8','foto-capa9']
+
+/*Nome dos filmes*/
+filmes = ["UMBRELLA ACADEMY","STAR WARS: A ASCENSÃO SKYWALKER","POKEMÓN",
+        "CAPITÃO AMÉRICA: GUERRA CIVIL","O HOBBIT: A BATLHA DOS CINCOS EXÉRCITOS",
+        "BREAKING BAD","HOUSE","SUPERNATURAL","SMALVILLE","VINGADORES: GUERRA INFINITA"]
+
+/*/*variável que será atribuída o nome de cada filme acima para exibir o título*/
+let filme
+
 
 let mostrarCapa = (id) => {
     if (document.getElementById("capas").style.display == "none"){
         document.getElementById("capas").style.display = "block"
-        escolherClasse(id).classList.add(capas[id])
+        mostrarTitulo(id)
+        mostrarClasse(id).classList.add(capas[id])
     }else{
         document.getElementById("capas").style.display = "none"
-        escolherClasse(id).classList.remove(capas[id])
+        ocultarTitulo()
+        ocultarClasse(id)
     }
 }
 
-let escolherClasse = (id) => {
-    if (id == 0){
-        return capa0
-    }else if(id == 1){
-        return capa1
-    }else if(id == 2){
-        return capa2
-    }else if(id == 3){
-        return capa3
-    }else if(id == 4){
-        return capa4
-    }else if(id == 5){
-        return capa5
-    }else if(id == 6){
-        return capa6
-    }else if(id == 7){
-        return capa7
-    }else if(id == 8){
-        return capa8
-    }else if(id == 9){
-        return capa9
+let mostrarTitulo = (id) => {
+    filme = document.createTextNode(filmes[id])
+    document.getElementById('titulo').appendChild(filme);
+}
+
+let ocultarTitulo = () => {
+    document.getElementById('titulo').removeChild(filme)
+}
+
+let mostrarClasse = (id) => {
+    capa = document.querySelector(capasHtml[id])
+    return capa
+}
+
+let ocultarClasse = (id) => {
+    for (i in capasHtml){
+        capa = document.querySelector(capasHtml[i])
+        capa.classList.remove(capas[i])
     }
 }
